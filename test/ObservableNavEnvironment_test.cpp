369f762b316289039ea097486d2c6da8ee140614
Kis Gergely Domonkos <vmt982@inf.elte.hu>
2021-03-17 14:27:46 +0100

    Default template paraméterek ObsNav tesztbe

--- test/ObservableNavEnvironment_test.cpp
+++ test/ObservableNavEnvironment_test.cpp
@@ -14,14 +14,16 @@ protected:
 
 TEST_F(ObservableNavEnvironmentTest, getVolumeAtPoint_x_y_z)
 {
-    ObservableNavEnvironment<Tile> env{3, 3, 3};
+    ObservableNavEnvironment<> env{3, 3, 3};
     for (int x = 0; x < 3; ++x)
         for (int y = 0; y < 3; ++y)
             for (int z = 0; z < 3; ++z)
             {
-                env.getBuffer()[env.getCoordAsIndex(x, y, z)] = std::make_unique<Tile>(Tile::Point{x, y, z});
-                EXPECT_EQ(env.getVolume(IObservableEnvironment::Point(x, y, z)).getPosition().getPosX(), x) << "[ERROR] Wrong coordinate X";
-                EXPECT_EQ(env.getVolume(IObservableEnvironment::Point(x, y, z)).getPosition().getPosY(), y) << "[ERROR] Wrong coordinate Y";
-                EXPECT_EQ(env.getVolume(IObservableEnvironment::Point(x, y, z)).getPosition().getPosZ(), z) << "[ERROR] Wrong coordinate Z";
+                env.getBuffer()[env.getCoordAsIndex(x, y, z)] =
+                    std::make_unique<decltype(env)::VolumeType>(Tile::Point{x, y, z});
+
+                EXPECT_EQ(env.getVolume(decltype(env)::Point(x, y, z)).getPosition().getPosX(), x) << "[ERROR] Wrong coordinate X";
+                EXPECT_EQ(env.getVolume(decltype(env)::Point(x, y, z)).getPosition().getPosY(), y) << "[ERROR] Wrong coordinate Y";
+                EXPECT_EQ(env.getVolume(decltype(env)::Point(x, y, z)).getPosition().getPosZ(), z) << "[ERROR] Wrong coordinate Z";
             }
 }
